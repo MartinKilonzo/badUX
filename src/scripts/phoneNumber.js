f65cb8843267a4b5308188b23776bf1f49a70666
MartinKilonzo <martinkilonzo2@gmail.com>
2017-04-03 23:25:08 -0400

    Swtiched from HSL hash to RGB hash

--- src/scripts/phoneNumber.js
+++ src/scripts/phoneNumber.js
@@ -1,18 +1,24 @@
 $(() => {
-  const formatPhoneNumber = numStr => `(${numStr.slice(0, 3)})-${numStr.slice(3, 6)}-${numStr.slice(6)}`;
+  const pad = (num, length) => {
+    let numStr = num.toString();
+    while (numStr.length < length) {
+      numStr = `0${numStr}`;
+    }
+    return numStr;
+  };
+
+  const shadeToNumber = (shade, length) => pad(Math.floor((shade / 256) * 10 ** length + 1) % (10 ** length), length);
 
   $('#bsc').on('click', () => {
     console.log('oh boy');
   });
   $('#bsc').colorpicker({
-    color: '#25aabd'
+    color: '#25aabd',
   }).on('changeColor', (e) => {
-    const hsl = e.color.toHSL();
-    const number = hsl.h * hsl.s * hsl.l * Math.pow(10, 12) % Math.pow(10, 10);
-    let numStr = Math.floor(number).toString();
-    while (numStr.length < 10) {
-      numStr = `0${numStr}`;
-    }
-    $('#pn').text(formatPhoneNumber(numStr));
+    const color = e.color.toRGB();
+    const areaCode = shadeToNumber(color.r, 3);
+    const regionCode = shadeToNumber(color.g, 3);
+    const localCode = shadeToNumber(color.b, 4);
+    $('#pn').text(`(${areaCode})-${regionCode}-${localCode}`);
   });
 });
